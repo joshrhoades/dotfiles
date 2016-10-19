ceb952ba626d5aa6ddff292ef4f6314d00ece247
Josh Rhoades <joshua.rhoades@gmail.com>
2016-10-19 12:57:14 -0700

    Updated brew cask to install specified fonts

--- installers/brew-cask.sh
+++ installers/brew-cask.sh
@@ -9,6 +9,7 @@ brew tap caskroom/versions
 
 # Install packages
 # Install apptrap first so we can cleanly remove anything that is added afterwards
+echo ":: BREW:CASKS INSTALLING APPS ::"
 apps=(
 	apptrap
 	1password
@@ -54,5 +55,29 @@ apps=(
 
 brew cask install "${apps[@]}"
 
+# Install fonts
+brew tap caskroom/fonts
+echo ":: BREW:CASKS: INSTALLING FONTS ::"
+fonts=(
+	font-input
+	font-droid-sans
+	font-roboto
+	font-source-sans-pro
+	font-awesome-terminal-fonts
+	font-fontawesome
+	font-octicons
+	font-icomoon
+	font-montserrat
+	font-lato
+	font-noto-sans
+	font-open-sans
+)
+brew cask install "${fonts[@]}"
+
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+echo ":: BREW:CASKS: UPDATING QUICK LOOK ::"
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
+
+# Remove outdated versions from the cellar
+echo ":: BREW:CASKS: CLEANING UP ::"
+brew cask cleanup
